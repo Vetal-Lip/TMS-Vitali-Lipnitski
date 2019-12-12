96827ee4a89e69a9c001bd29d6680b11ce78e158
Vitali <terrmit574@gmail.com>
2019-12-12 22:35:11 +0300

    weather-app + dinamic map (last version)

--- weather-app+dinamic map/index.js
+++ weather-app+dinamic map/index.js
@@ -8,8 +8,21 @@ const buttonFind = document.getElementById('button');
 const tbody = document.querySelector('#tbody');
 let mapId = document.getElementById('map');
 const storage = new Map();
+const mapMarkers = [];
 let map;
 let marker;
+
+const key = 'pk.65c93667fe548705277ec2034036cbc4';
+const streets = L.tileLayer.Unwired({key: key, scheme: "streets"});
+map = L.map('map', {
+    center: [52.5170365, 13.3888599], //map loads with Berlin as center
+    zoom: 5,
+    layers: [streets] // Show 'streets' by default
+});     
+L.control.scale().addTo(map);
+L.control.layers({
+    "Streets": streets
+}).addTo(map); 
 
 function renderTbody() {
     tbody.innerHTML = '' 
@@ -38,39 +51,21 @@ buttonFind.addEventListener('click', function(){
         console.log(data)
         storage.set(name, [name, country, temperature]);
         renderTbody();
-        
-        
-        if (!mapId.innerHTML) {
-          const key = 'pk.65c93667fe548705277ec2034036cbc4';
-            const streets = L.tileLayer.Unwired({key: key, scheme: "streets"});
-             map = L.map('map', {
-                center: [52.5170365, 13.3888599], //map loads with Berlin as center
-                zoom: 5,
-                layers: [streets] // Show 'streets' by default
-                });     
-             L.control.scale().addTo(map);
-             L.control.layers({
-                "Streets": streets
-            }).addTo(map); 
-            marker = L.marker([lat, lon]).addTo(map);
-            marker.bindPopup(`${[lat, lon]}`).openPopup();
-                        
-        } else {
-           marker = L.marker([lat, lon]).addTo(map);
-           marker.bindPopup(`${[lat, lon]}`).openPopup();
-                    
-        };
-        console.log(map);
-       
+
+        marker = L.marker([lat, lon]).addTo(map);
+        marker.bindPopup(`${[lat, lon]}`).openPopup();
+        mapMarkers.push(marker);
+        console.log(mapMarkers)
     })
 })
 
-
-
 let buttonClear = document.getElementById('clear');
-buttonClear.addEventListener('click', function(){
+buttonClear.addEventListener('click', function(event){
+    
     storage.clear();
     tbody.innerHTML = ' ';
-    map.remove();
-    console.log(storage)
+    mapMarkers.forEach(function(item){
+        map.removeLayer(item);
+    })
+
 })
